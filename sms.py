15ee1263288fda07b78f90da04a691bc1126803b
KD0KKV <kd0kkv@arrl.com>
2016-09-22 09:31:36 -0500

    sms fixes

--- sms.py
+++ sms.py
@@ -36,20 +36,39 @@ def do_login():
 
 do_auth()
 
-send_msg = "zomgwaffles"
+####
+
+send_msg = "msg"
 send_dstid = "3120022"
 send_calltype = "0"
 send_srcid = "9998"
 send_format = "1"
 send_tdma_channel = "0"
 
-encoded = "".join([str('00' + x) for x in re.findall('..',binascii.hexlify(send_msg))] )
+def do_send_sms():
+ encoded = "".join([str('00' + x) for x in re.findall('..',binascii.hexlify(send_msg))] )
 
-if len(encoded) > 150:
- print("Message too long")
- exit()
+ if len(encoded) > 150:
+  print("Message too long")
+  exit()
 
-post = { 'token': tok, 'digest': digest, 'send_dstid': send_dstid, 'send_calltype': send_calltype, 'send_srcid': send_srcid, 'send_format': send_format, 'send_tdma_channel': send_tdma_channel, 'send_msg': encoded.upper() }
-print(str(post))
-r = requests.post("http://"+ip+"/status-dmrsms.cgi", json=post)
-print(r.text)
+ post = { 'token': tok, 'digest': digest, 'send_dstid': send_dstid, 'send_calltype': send_calltype, 'send_srcid': send_srcid, 'send_format': send_format, 'send_tdma_channel': send_tdma_channel, 'send_msg': encoded.upper() }
+ r = requests.post("http://"+ip+"/status-dmrsms.cgi", json=post)
+ if int(json.loads(r.text)['send_success']) != 1:
+  print("not sent")
+
+
+def do_recieve_sms():
+ post = { 'token': tok, 'digest': digest }
+ r = requests.post("http://"+ip+"/status-dmrsms.cgi", json=post)
+ if int(json.loads(r.text)['rx_msg_valid']) == 1:
+  print("Message recieved from: " + str(json.loads(r.text)['rx_msg_srcid']) )
+  print("Encoded message: " + json.loads(r.text)['rx_msg'] )
+
+if len(sys.argv) > 2:
+ print(sys.argv[1:])
+ send_dstid = str(sys.argv[1])
+ send_msg = str(sys.argv[2:])
+ do_send_sms()
+else:
+ do_recieve_sms()
