4afce5ada97d2fbdbeb983652da987534eb48249
skot <skot@hostname>
2016-09-22 20:03:10 -0500

    cleanup and fixes

--- README.md
+++ README.md
@@ -3,10 +3,22 @@ Interact with an openSpot SharkRF via the http json api.
 For now, only a few options are implemented for changing settings. I will work on adding more or all of the settings.
 Make sure you set your password and host/ip address if you've changed it from the default.
 
-./shark.py
 
-display status and exit
+There is an example script:
 
-./shark.py -h
+./info.py
 
-show command line arguments
+display status of the openSpot and exit
+
+./info.py -h
+
+show command line arguments to change some options
+
+Here is another example:
+
+'''
+#!/usr/bin/python
+import re, binascii, shark
+shark.do_checkauth()
+shark.do_send_sms( '3120022', 'hey there' )
+'''
--- shark.py
+++ shark.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python
-import sys, getopt, requests, json
+import sys, getopt, requests, json, re, binascii
 from hashlib import sha256
 
 password = 'openspot'
 ip = 'openspot.local'
 tmp = '/tmp/.shark.auth'
 
-def show_usage():
- print('''
-  usage: ./shark.py
-   display status
-
-  usage: ./shark.py -g <talkgroup> -m <mode> -f <frequency>
-   -m   idle/dmr/c4fm/dstar
-   -g   dmr talkgroup number
-   -f   frequency in MHz
-  
-  ex:
-   ./shark.py
-   ./shark.py -g 4639 -m c4fm
-   ./shark.py -m dmr -f 433.1
-   ./shark.py -g 0
-  ''')
-
-def do_auth():
+def do_checkauth():
  global tok, digest, post
  try:
    f = open(tmp, 'r')
@@ -48,19 +31,18 @@ def do_login():
   exit()
  else:
   f = open(tmp, 'w')
-  f.write(tok + '\n' + digest)
+  f.write(tok + "\n" + digest)
   f.close
 
 def get_freq():
- global rx, tx
  rfreq = requests.post("http://"+ip+"/modemfreq.cgi", json=post)
  rx = rfreq.json()["rx_frequency"]
  tx = rfreq.json()["tx_frequency"]
  rx = str(rx / 1000000.0) + "MHz"
  tx = str(tx / 1000000.0) + "MHz"
+ return{ 'rx': rx, 'tx': tx }
 
 def get_status():
- global room, status
  rstatus = requests.post("http://"+ip+"/status.cgi", json=post)
  room = rstatus.json()["connected_to"]
  status = rstatus.json()["status"]
@@ -80,9 +62,9 @@ def get_status():
   status = "Modem firmware upgrade in progress"
  else:
   status = "API ERROR: unknown status code"
+ return{ 'status': status, 'room': room }
 
 def get_mode():
- global mode, submode
  rmode = requests.post("http://"+ip+"/modemmode.cgi", json=post)
  mode = rmode.json()["mode"]
  if mode == 0:
@@ -108,9 +90,9 @@ def get_mode():
   submode = "DMR BS"
  else:
   submode = "API ERROR: unknown sub mode"
+ return{ 'mode': mode, 'submode': submode }
 
 def get_connector():
- global connector
  rconnector = requests.post("http://"+ip+"/connector.cgi", json=post)
  connector = rconnector.json()["active_connector"]
  if connector == 0:
@@ -137,39 +119,22 @@ def get_connector():
   connector = "YSF Reflector"
  else:
   connector = "API ERROR: unknown connector type" 
+ return(connector)
 
-def args(argv):
- try:
-  opts, args = getopt.getopt(argv,"hf:m:g:")
- except getopt.GetoptError:
-  show_usage()
- for opt, arg in opts:
-  if opt == '-h':
-   show_usage()
-   exit()
-  elif opt == '-f':
-   set_freq(arg)
-  elif opt == '-m':
-   set_mode(arg)
-  elif opt == '-g':
-   set_talkgroup(arg)
-
-def set_talkgroup(argv):
- post = { 'token': tok, 'digest': digest, 'new_autocon_id': argv, 'new_c4fm_dstid': argv, 'new_reroute_id': argv }
+def set_talkgroup(new_group):
+ post = { 'token': tok, 'digest': digest, 'new_autocon_id': new_group, 'new_c4fm_dstid': new_group, 'new_reroute_id': new_group }
  de = requests.post("http://"+ip+"/homebrewsettings.cgi", json=post)
- if int(json.loads(de.text)['changed']) != 1:
-  print("TALKGROUP ERROR: make sure " + argv + " is correct")
- else:
-  print("Changing to talkgroup " + argv)
+ return(json.loads(de.text)['changed'])
 
-def set_freq(argv):
- f = float(argv) * 1000000
- post = { 'token': tok, 'digest': digest, 'new_rx_freq': f, 'new_tx_freq': f }
- dc = requests.post("http://"+ip+"/homebrewsettings.cgi", json=post)
- if int(json.loads(dc.text)['changed']) != 1:
-  print("FREQUENCY ERROR: make sure " + argv + " is correct")
+def set_freq( new_rx_freq, new_tx_freq = 1 ):
+ new_rx_freq = float(new_rx_freq) * 1000000
+ if new_tx_freq == 1:
+  new_tx_freq = new_rx_freq
  else:
-  print("Changing to frequency " + argv)
+  new_tx_freq = float(new_tx_freq) * 1000000
+ post = { 'token': tok, 'digest': digest, 'new_rx_freq': new_rx_freq, 'new_tx_freq': new_tx_freq }
+ dc = requests.post("http://"+ip+"/homebrewsettings.cgi", json=post)
+ return( int(json.loads(dc.text)['changed']) ) 
 
 def set_mode(argv):
  if argv == "dmr":
@@ -183,35 +148,30 @@ def set_mode(argv):
  elif argv == "idle":
   newmode = 0
  else:
-  print("MODE ERROR: try one of dmr, dstar, c4fm, raw, idle")
+  return("MODE ERROR: try one of dmr, dstar, c4fm, raw, idle")
  if newmode == 2:
   post = { 'token': tok, 'digest': digest, 'new_mode': newmode, 'new_submode': '1' }
  else:
   post = { 'token': tok, 'digest': digest, 'new_mode': newmode, 'new_submode': '0' }
  dm = requests.post("http://"+ip+"/modemmode.cgi", json=post)
  if int(json.loads(dm.text)['changed']) != 1:
-  print("MODE ERROR: cannot change mode")
- else:
-  print("Changing mode to " + argv)
+  return("MODE ERROR: cannot change mode")
 
-def show_info():
- get_mode()
- get_freq()
- get_connector()
- get_status()
- print("Status: " + status)
- print("Connector: " + connector)
- if rx == tx:
-  print("Frequency: " + tx)
- else:
-  print("Recieve Freq: " + rx)
-  print("Transmit Freq: " + tx)
- print("Mode: " + mode + " (" + submode +")")
- print("Talkgroup: " + room)
+def do_send_sms( dstid, msg ):
+ send_calltype = "0"
+ send_srcid = "9998"
+ send_format = "1" #MD-380/390
+ send_tdma_channel = "0"
+ encoded = "".join([str('00' + x) for x in re.findall('..',binascii.hexlify(msg))] )
+ if len(encoded) > 150:
+  return("Message too long")
+ post = { 'token': tok, 'digest': digest, 'send_dstid': dstid, 'send_calltype': send_calltype, 'send_srcid': send_srcid, 'send_format': send_format, 'send_tdma_channel': send_tdma_channel, 'send_msg': encoded.upper() }
+ requests.post("http://"+ip+"/status-dmrsms.cgi", json=post)
 
-
-
-do_auth()
-args(sys.argv[1:])
-if len(sys.argv) <= 1:
- show_info()
+def do_recieve_sms():
+ post = { 'token': tok, 'digest': digest }
+ r = requests.post("http://"+ip+"/status-dmrsms.cgi", json=post)
+ if int(json.loads(r.text)['rx_msg_valid']) == 1:
+  sender = str(json.loads(r.text)['rx_msg_srcid'])
+  decoded = binascii.unhexlify(json.loads(r.text)['rx_msg'])
+  return(sender, decoded)
